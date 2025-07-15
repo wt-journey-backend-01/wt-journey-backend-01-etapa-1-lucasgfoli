8b6256a47b3bb21a25431a944363c5c656782576
Lucas Oliveira <Lucas.Oliveira@geothra.com>
2025-07-15 08:46:20 -0300

    ajuste das pastas

--- server.js
+++ server.js
@@ -1,33 +1,71 @@
 const express = require('express')
 const path = require('path')
 
-const app = express();
-const PORT = 3000;
+const app = express()
+const PORT = 3000
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded({extended: true})) //Para tratar dados que vem do form
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-let ultimoContato = null
-
-app.get('/not-found', (req, res) => {
-    res.status(404).send('<h1>Page not found</h1>')
-})
-
-app.post('/contato', (req, res) => {
-    ultimoContato = req.body
-
-    res.redirect('/contato-recebido')
-})
-
-app.get('/contato-recebido', (req, res) => {
-    if (!ultimoContato){
-        return res.redirect('/not-found')
-    }
-
-    const { name, email} = ultimoContato
-    res.send(``)
+//Define o caminho da rota '/' GET e define os parametros request e response
+//Para lidar com a requisição do cliente e enviar uma resposta do servidor
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
 app.listen(PORT, () => {
-    console.log(`Servidor da DevBurger rodando em localhost:${PORT}`)
+    console.log(`Servidor rodando na porta ${PORT}`)
 })
+
+app.get('/sugestao', (req, res) => {
+    const { nome, ingredientes } = req.query
+
+    if (!nome || !ingredientes)
+        return res.status(400).send('Parâmetros ausentes')
+
+    res.send(`<script>alert("Obrigado pela sua sugestão ${nome}! Você sugeriu ${ingredientes}"); window.location.href = "/page_location"; </script>`)
+})
+
+app.get('/contato', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'contato.html'))
+})
+
+app.post('/contato', (req, res) => {
+    const { nome, email, assunto, mensagem } = req.body
+
+    if (!nome || !email || !assunto || !mensagem)
+        return res.status(400).send('Todos os campos são obrigatórios!')
+
+    res.send(`
+        <h1>Contato Recebido</h1>
+        <p>Nome: ${nome}</p>
+        <p>Email: ${email}</p>
+        <p>Assunto: ${assunto}</p>
+        <p>Mensagem: ${mensagem}</p>
+        <a href="/">Voltar</a>
+        `)
+
+})
+
+app.get('/api/lanches', (req, res) => {
+    let lanches = [
+        {
+            "id": 1,
+            "nome": "DevBurger Clássico",
+            "ingredientes": "Pão brioche, Carne 150g, Queijo cheddar, Alface americana, Tomate fresco, Molho especial"
+        },
+        {
+            "id": 2,
+            "nome": "Burger de Bacon",
+            "ingredientes": "Pão australiano, Carne 180g, Queijo prato, Bacon crocante, Cebola caramelizada, Molho barbecue"
+        },
+        {
+            "id": 3,
+            "nome": "Commit Veggie",
+            "ingredientes": "Pão integral, Burger de grão de bico, Queijo vegano, Rúcula, Tomate seco, Maionese de ervas"
+        }
+    ]
+
+    return res.json(lanches)
+}) 
